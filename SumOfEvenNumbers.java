46e7e1ed438b45606e16b1a5bb3f01af51e74c6a
avishkar <avishkarkurkute701@gmail.com>
2024-12-22 09:53:44 +0530

    first commit

---
+++ SumOfEvenNumbers.java
@@ -0,0 +1,15 @@
+public class SumOfEvenNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        int count = 0;
+        
+       
+        for (int i = 2; count < 100; i += 2) {
+            sum += i;
+            count++;
+        }
+        
+        
+        System.out.println("The sum of the first 100 even numbers is: " + sum);
+    }
+}"# sumofnumber" 
